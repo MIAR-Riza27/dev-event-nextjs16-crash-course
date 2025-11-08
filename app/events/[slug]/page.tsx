8219019d3f878600d99198008f51c9d6fa7630bf
MIAR-Riza27 <ipnualvariza@gmail.com>
2025-11-08 12:53:35 +0700

    vercel: 2

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,14 +2,12 @@ import EventDetails from "@/components/EventDetails";
 import { Suspense } from "react";
 
 export default async function EventDetailsPage({ params }: { params: Promise<{ slug: string }> }) {
-  const slug = params.then((p) => p.slug);
-
   return (
     <main>
       <Suspense fallback={ 
         <div>Loading...</div>
       }>
-        <EventDetails params={slug} />
+        <EventDetails params={params} />
       </Suspense>
     </main>
   )
